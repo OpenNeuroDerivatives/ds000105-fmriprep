e12c4116523e13f449c579db035a60c111939ab1
jbwexler <jbwexler@tutanota.com>
2022-02-18 15:56:15 -0600

    [DATALAD] Recorded changes

--- sourcedata/freesurfer/sub-2/scripts/recon-all.cmd
+++ sourcedata/freesurfer/sub-2/scripts/recon-all.cmd
@@ -1,1 +1,1 @@
-/annex/objects/MD5E-s8127--52624749c305e8c72fbad9c0f2b4f402.cmd
+../../../../.git/annex/objects/4m/g9/MD5E-s8127--52624749c305e8c72fbad9c0f2b4f402.cmd/MD5E-s8127--52624749c305e8c72fbad9c0f2b4f402.cmd
